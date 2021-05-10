0f20ca4cd34ca9cdaf3b09914f6f4a03b497a6d0
borislav12322 <borislavizmestiev@gmail.com>
2021-05-10 15:36:38 +0300

    Add Scripts in Other file

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+
+        $(document).ready(function () {
+            $('.slider').slick();
+        });
+    
+
+    
+        $(document).ready(function () {
+            $('.gallery__photos').magnificPopup({
+                delegate: 'a',
+                type: 'image',
+                tLoading: 'Loading image #%curr%...',
+                mainClass: 'mfp-img-mobile',
+                gallery: {
+                    enabled: true,
+                    navigateByImgClick: true,
+                    preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+                },
+                image: {
+                    tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+                    titleSrc: function (item) {
+                        return item.el.attr('title');
+                    }
+                }
+            });
+        });
+    
+
+    
+        $(document).ready(function () {
+            $('.header__btn-burger').click(function (event) {
+                $('.header__btn-burger, .header__mobile-menu, .mobile-menu').toggleClass('active');
+                $('body').toggleClass('lock');
+            });
+        });
+    
